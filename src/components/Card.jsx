a2cb98e29dc710ba00b25b78c9b82c3fcbbc6ea2
Paul Lindén <linden.91.pl@gmail.com>
2025-11-01 15:24:20 +0100

    updated UI on cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,15 +3,15 @@ import PropTypes from 'prop-types';
 
 function Card({ imageSrc, title, description }) {
     return (
-        <div className="min-h-[280px] w-[229px] rounded-lg shadow-md overflow-hidden bg-white">
+        <div className="min-h-[280px] w-[229px] rounded-lg border-1 border-black  overflow-hidden hover:bg-[#fafafb] cursor-pointer text-center">
             <img
                 src={imageSrc}
                 alt={title}
                 className="w-full h-32 object-cover"
             />
             <div className="p-3">
-                <h2 className="text-lg font-semibold text-gray-800">{title}</h2>
-                <p className="mt-1 text-sm text-gray-600">{description}</p>
+                <h2 className="text-lg font-semibold text-gray-800 center">{title}</h2>
+                <p className="mt-1 text-sm text-gray-600 ">{description}</p>
             </div>
         </div>
     );
--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,34 +1,34 @@
 //CardContainer
 import Card from "./Card";
-
+import img from '../assets/swopti.svg'
 function CardContainer() {
     const cards = [
         {
-            imageSrc: 'https://via.placeholder.com/300x200?text=Card+1',
+            imageSrc: img,
             title: 'Kort 1',
             description: 'Detta är en beskrivning för det första kortet.',
         },
         {
-            imageSrc: 'https://via.placeholder.com/300x200?text=Card+2',
+            imageSrc: img,
             title: 'Kort 2',
             description: 'Detta är en beskrivning för det andra kortet.',
         },
         {
-            imageSrc: 'https://via.placeholder.com/300x200?text=Card+3',
+            imageSrc: img,
             title: 'Kort 3',
             description: 'Detta är en beskrivning för det tredje kortet.',
         },
         {
-            imageSrc: 'https://via.placeholder.com/300x200?text=Card+4',
+            imageSrc: img,
             title: 'Kort 4',
             description: 'Detta är en beskrivning för det fjärde kortet.',
         },
     ];
 
     return (
-        <article class="flex flex-col items-center justify-center flex">
+        <article className="flex flex-col items-center justify-center flex">
 
-            <h1 class="text-align: center; font-bold text-2xl mb-10">Välj glastyp</h1>
+            <h1 className="text-center; font-bold text-2xl mb-10">Välj glastyp</h1>
 
             <section className="flex flex-row flex-wrap justify-center gap-4 max-h-80">
                 {cards.map((card, index) => (
