25ec1de853f9672a36d29f2e1b080a4052bee74f
Paul Lindén <linden.91.pl@gmail.com>
2025-11-01 11:53:10 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div class="h-screen w-screen bg-[#f1e4de]">
+        
+    </div>
+  )
+}
+
+export default App
+//#f1e4de  oklch(88.5% 0.062 18.334)
