a09164e1e01c34e588055bc99ae98fd121a0d8de
Paul Lindén <linden.91.pl@gmail.com>
2025-11-01 12:56:01 +0100

    first take on App, only for dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import { useState } from 'react'
+import CardContainer from './components/CardContainer';
 
 function App() {
 
+  
+
   return (
-    <div class="h-screen w-screen bg-[#f1e4de]">
-        
+    <div className="min-h-screen w-screen bg-[#f1e4de] flex flex-col justify-between gap-6 p-4">
+      <section className="h-24 w-full bg-gray-200 flex items-center justify-center">
+        <h2 className="text-2xl font-semibold text-gray-800">Toppsektion</h2>
+      </section>
+
+      <CardContainer></CardContainer>
+
+      <section className="h-24 w-full bg-gray-200 flex items-center justify-center">
+        <h2 className="text-2xl font-semibold text-gray-800">Bottenssektion</h2>
+      </section>
     </div>
-  )
+  );
 }
 
 export default App
-//#f1e4de  oklch(88.5% 0.062 18.334)
