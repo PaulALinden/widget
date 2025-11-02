6c8f7fa5e4f135000a073d645f71c6e1a95dcb82
Paul Lindén <linden.91.pl@gmail.com>
2025-11-02 20:19:40 +0100

    API

--- src/api/productsApi.js
+++ src/api/productsApi.js
@@ -3,8 +3,6 @@ import apiClient from './client';
 export const productsApi = {
     getStoreConfig: (storeId) => {
         return apiClient.get(`/stores/${storeId}/config`);
-        // apiClient har redan baseURL, så blir:
-        // GET http://localhost:3001/api/stores/store_1/config
     },
 
     calculatePrice: (storeId, selections) => {
