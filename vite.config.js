7b7ef46b02dd83008c96455ec45cac3cebd7c401
Paul Lindén <linden.91.pl@gmail.com>
2025-11-05 19:21:14 +0100

    build

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,26 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  
-})
+  plugins: [react(), tailwindcss()],
+  define: {
+    'process.env': {}  // ← Lägg till denna
+  },
+  build: {
+    lib: {
+      entry: './src/widget.jsx',
+      name: 'OpticalWidget',
+      fileName: 'optical-widget',
+      
+    },
+    rollupOptions: {
+      output: {
+        assetFileNames: 'optical-widget.[ext]'
+      }
+    }
+  },
+  preview: {
+    port: 8080
+  }
+});
